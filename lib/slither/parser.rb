ba7ecc5c6af2ddaf512f6240bb5324da14a8f02f
Julian Russell <jr.plusplus@gmail.com>
2014-09-25 10:31:49 +1000

    Added quick option to accept LONGER lines, but not shorter

--- lib/slither/parser.rb
+++ lib/slither/parser.rb
@@ -1,13 +1,16 @@
 class Slither
   class Parser
-        
+
     def initialize(definition, file_io)
       @definition = definition
       @file = file_io
       # This may be used in the future for non-linear or repeating sections
       @mode = :linear
+      @length_validation = :strict
     end
-    
+
+    attr_accessor :length_validation
+
     def parse(collector = nil)
       collector ||= default_collector
 
@@ -23,37 +26,37 @@ class Slither
       end
       collector.finished
     end
-    
+
     def parse_by_bytes
       parsed = {}
-      
+
       all_section_lengths = @definition.sections.map{|sec| sec.length }
       byte_length = all_section_lengths.max
       all_section_lengths.each { |bytes| raise(Slither::SectionsNotSameLengthError,
           "All sections must have the same number of bytes for parse by bytes") if bytes != byte_length }
-      
+
       while record = @file.read(byte_length)
-        
+
         unless remove_newlines! && byte_length == record.length
           parsed_line = parse_for_error_message(record)
-          raise(Slither::LineWrongSizeError, "Line wrong size: No newline at #{byte_length} bytes. #{parsed_line}") 
+          raise(Slither::LineWrongSizeError, "Line wrong size: No newline at #{byte_length} bytes. #{parsed_line}")
         end
-    
+
         record.force_encoding @file.external_encoding
-        
+
         @definition.sections.each do |section|
           if section.match(record)
             collector.process_record(section.name, section.parse(record))
           end
         end
       end
-      
+
       @definition.sections.each do |section|
         raise(Slither::RequiredSectionNotFoundError, "Required section '#{section.name}' was not found.") unless parsed[section.name] || section.optional
       end
       parsed
     end
-    
+
     private
 
       def default_collector
@@ -61,12 +64,20 @@ class Slither
       end
 
       def validate_length(line, section)
-        if line.length != section.length
+        unless length_valid?(line, section)
           parsed_line = parse_for_error_message(line)
-          raise Slither::LineWrongSizeError, "Line wrong size: (#{line.length} when it should be #{section.length}. #{parsed_line})" 
+          raise Slither::LineWrongSizeError, "Line wrong size: (#{line.length} when it should be #{section.length}. #{parsed_line})"
         end
       end
-      
+
+      def length_valid?(line, section)
+        if length_validation == :strict
+          line.length == section.length
+        else
+          line.length >= section.length
+        end
+      end
+
       def remove_newlines!
         return true if @file.eof?
         b = @file.getbyte
@@ -77,13 +88,13 @@ class Slither
           return false
         end
       end
-      
+
       def newline?(char_code)
         # \n or LF -> 10
         # \r or CR -> 13
         [10, 13].any?{|code| char_code == code}
       end
-      
+
       def parse_for_error_message(line)
         parsed = ''
         line.force_encoding @file.external_encoding
@@ -94,6 +105,6 @@ class Slither
         end
         parsed
       end
-      
+
   end
 end
